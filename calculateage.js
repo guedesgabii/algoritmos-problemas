ecb6cb674c933fc5740edaa6c6b7c503d5292e7e
Avishek <avishekmishra56@gmail.com>
2024-08-23 15:44:48 +0530

    Calculate age optimization

--- calculateage.js
+++ calculateage.js
@@ -1,21 +1,20 @@
 function calculateAge(birthDate, currentDate) {
- 
     let [birthDay, birthMonth, birthYear] = birthDate.split('/').map(Number);
-  
     let [currentDay, currentMonth, currentYear] = currentDate.split('/').map(Number);
 
- 
     let ageYears = currentYear - birthYear;
     let ageMonths = currentMonth - birthMonth;
     let ageDays = currentDay - birthDay;
 
-
+    // Adjust for negative days
     if (ageDays < 0) {
         ageMonths--;
-        ageDays += new Date(currentYear, currentMonth - 1, 0).getDate();
+        // Get the number of days in the previous month
+        let daysInPrevMonth = new Date(currentYear, currentMonth - 1, 0).getDate();
+        ageDays += daysInPrevMonth;
     }
 
-
+    // Adjust for negative months
     if (ageMonths < 0) {
         ageYears--;
         ageMonths += 12;
@@ -24,8 +23,6 @@ function calculateAge(birthDate, currentDate) {
     return `Present Age = Years: ${ageYears}  Months: ${ageMonths}  Days: ${ageDays}`;
 }
 
-
 let birthDate = "07/09/1996";
 let currentDate = "07/12/2017";
 console.log(calculateAge(birthDate, currentDate));
-
